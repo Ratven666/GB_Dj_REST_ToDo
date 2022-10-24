5ff83318790c256f86d45ee28898d099921ca3ff
VMG <Ratven@yandex.ru>
2022-10-24 21:21:23 +0300

    HomeWork8

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3001",
-    "http://localhost:3001",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 # Application definition
 
